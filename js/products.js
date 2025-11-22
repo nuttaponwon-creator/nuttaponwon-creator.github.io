7aa2c0a4af56d656c1ae9a7e0e25cfc38e0ceb6b
nuttaponwon-creator <nuttapon.won@spumail.net>
2025-11-23 01:48:04 +0700

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,103 @@
+const products = [
+  {
+    id: 1,
+    name: "แจ็คเก็ตยีนส์",
+    price: 450,
+    sizes: [ "S","M", "L", "XL"],
+    img: "img/แจ็คเก็ตยีนส์.jpg",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: "man",
+    category: "other",
+    badge:"ใหม่"
+  },
+  {
+    id: 2,
+    name: "ชุดเซ็ตไหมพรม+กระโปรง",
+    price: 990,
+    sizes: ["S","M", "L"],
+    img: "img/ชุดเซ็ตไหมพรม.jpg",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: "woman",
+    category: "set",
+    badge:"ขายดี"
+  },
+  {
+    id: 3,
+    name: "เสื้อเชิ้ต",
+    price: 199,
+    sizes: ["M", "L"],
+    img: "img/เสื้อเชิ้ต.png",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: "man",
+    category: "shirt",
+    badge:"ขายดี"
+  },
+   {
+    id: 4,
+    name: "เดรส",
+    price: 490,
+    sizes: ["S", "M", "L", "XL" ],
+    img: "img/เดรส.jpg",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: "woman",
+    category: "other",
+    badge:"ใหม่"
+  },
+   {
+    id: 5,
+    name: "กางเกงยีนส์",
+    price: 399,
+    sizes: ["S", "M", "L", "XL" ],
+    img: "img/กางเกงยีนส์.png",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: "man",
+    category: "pants",
+    badge:"ขายดี"
+  },
+   {
+    id: 6,
+    name: "หมวกแก๊ป",
+    price: 150,
+    sizes: [ "S","M", "L"],
+    img: "img/หมวกแก๊ป.jpg",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: ["man","woman"],
+    category: "hat",
+    badge:"ใหม่"
+  },
+  {
+    id: 7,
+    name: "คาร์ดิแกน",
+    price: 200,
+    sizes: [ "S","M", "L"],
+    img: "img/คาดิแกน.jpg",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: ["man","woman"],
+    category: "other",
+    badge:"ขายดี"
+  },
+  {
+    id: 8,
+    name: "หมวก รด รุ่นลิมิเต็ด",
+    price: 999,
+    sizes: [ "S","M", "L"],
+    img: "img/หมวก.png",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: "man",
+    category: "hat",
+    badge:"ลิมิเต็ด"
+  },
+  {
+    id: 9,
+    name: "เสื้อไดโนพาร์ค",
+    price: 99,
+    sizes: ["S", "M", "L"],
+    img: "img/เสื้อยืด.jpg",
+    description: "สวมใส่สบาย สัมผัสเนียนนุ่ม ดูแลรักษาง่าย",
+    gender: ["man","woman"],
+    category: "tshirt",
+    badge:"ใหม่"
+  },
+  
+  
+];
